6b22732180e29158e3d8d2fa02c8418c7dcac0d3
DiegoFatVega <fatvegadiego@gmail.com>
2025-07-08 18:56:51 +0200

    Adds ticket price calculation with age-based discounts
    Implements logic to calculate ticket prices based on kilometers traveled and applies discounts for minors and seniors. Introduces constants for price per kilometer and discount rates to improve code readability and maintainability.

--- main.js
+++ main.js
@@ -2,10 +2,37 @@
 
 //creo una variabile per salvare il numero di KM da percorrere
 //creo una variabile per salvare l`età del passeggero
+
 //dichiaro una variabile per salvare  il prezzo del biglietto
 //creo una variabile per salvare il prezzo al Km (0.21)
 
+//prezzo_standard = prezzo_standard  * 0.8
+//prezzo_standard = prezzo_standard  * 0.6
+//una variabile per salvare il 20% di sconto
+//creo una variabile dove salvare il 20%
+//applico lo sconto del 20%
+
+
 //#elaborazione
-
-
+//calcolo il prezzo iniziale del biglietto (senza sconti)
+//verifico se l`età del passeggero è minore di 18, applico il 20% di sconto
+//altrimenti verifico se è maggiore di 65, allora applico
 //#OUTPUT
+
+const passenger_age = 10;
+const Km = 10 //number(prompt(`type the km you esnt to do in journey`))
+
+const PRICE_PER_KM  = 0.21;
+const minor_discount = 0.2;
+const senior_discount = 0.0;
+
+ticket_price = km * PRICE_PER_KM
+console.log(ticket_price);
+
+if(passenger_age < 18){
+    ticket_price = ticket_price * minor_discount;
+}
+else{
+    ticket_price = ticket_price * senior_discount;
+}
+
