cc5ee0b02dece97fb57af14df3d02e86877708e6
DiegoFatVega <fatvegadiego@gmail.com>
2025-07-08 18:42:13 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+//#preparazione
+
+//creo una variabile per salvare il numero di KM da percorrere
+//creo una variabile per salvare l`età del passeggero
+//dichiaro una variabile per salvare  il prezzo del biglietto
+//creo una variabile per salvare il prezzo al Km (0.21)
+
+//#elaborazione
+
+
+//#OUTPUT
